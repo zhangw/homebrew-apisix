97d7c89e92addbd8ad097dfe0c0dcd71c68dccaa
张文 <jimokanghanchao@gmail.com>
2022-12-07 22:10:31 +0800

    chore: trying customized patch

--- Formula/apisix-base.rb
+++ Formula/apisix-base.rb
@@ -1,5 +1,5 @@
 require 'etc'
-class Apisix < Formula
+class ApisixBase < Formula
     desc "Apache APISIX based upon Openresty, and also some patches would be applied."
     homepage "https://github.com/api7/apisix-build-tools"
     OPENRESTY_VERSION = "1.21.4.1".freeze
@@ -8,10 +8,10 @@ class Apisix < Formula
     sha256 "0c5093b64f7821e85065c99e5d4e6cc31820cfd7f37b9a0dec84209d87a2af99"
     # ngx_multi_upstream_module patch
     patch do
-        url "https://github.com/api7/ngx_multi_upstream_module/archive/refs/tags/1.1.1.tar.gz"
-        sha256 "2a2cf4bf3e137651ab0e93af9c2c7488578ec3cb7e387dcfa0df3677bdb9f3da"
-        directory "ngx_multi_upstream_module-1.1.1"
-        apply -d "openresty-#{OPENRESTY_VERSION}/bundle/nginx-1.21.4" "nginx-1.21.4.patch"
+        url ""
+        sha256 "980fcc6e845587232a3d5263dab4e2fd21b94ebf6603028ce9dcf43736292bba"
+        #directory "ngx_multi_upstream_module-1.1.1"
+        apply "openresty-#{OPENRESTY_VERSION}/bundle/nginx-1.21.4" "nginx-1.21.4.patch"
     end
 
     def install
